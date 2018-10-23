c9f0428d44f06c4d03c65d1211d7f4f7bfdabc6e
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2018-10-24 07:38:58 +1100

    Build for OSX first

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,16 +4,6 @@ LINUX_LOG_FILE=$UNITY_BUILD_DIR/linux-build.log
 OSX_LOG_FILE=$UNITY_BUILD_DIR/osx-build.log
 WINDOWS_LOG_FILE=$UNITY_BUILD_DIR/windows-build.log
 
-echo "Attempting to build for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe"
-  -quit | tee "$WINDOWS_LOG_FILE"
-
 echo "Attempting to build for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity
   -batchmode
@@ -33,3 +23,13 @@ echo "Attempting to build for Linux"
   -projectPath $(pwd)
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe"
   -quit | tee "$LINUX_LOG_FILE"
+
+echo "Attempting to build for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity
+  -batchmode
+  -nographics
+  -silent-crashes
+  -logFile $(pwd)/unity.log
+  -projectPath $(pwd)
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe"
+  -quit | tee "$WINDOWS_LOG_FILE"
